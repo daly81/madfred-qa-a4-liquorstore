bee4c16e314a439d78dbd2a466a4a2b898a13e36
daly81 <idaly81@gmail.com>
2013-09-12 09:19:56 -0400

    Assignment 4 Ian Daly

--- src/AgeChecker.java
+++ src/AgeChecker.java
@@ -1,15 +1,35 @@
+import javax.swing.JOptionPane; 
+
 
 public class AgeChecker {
 
 	public static void main(String[] args) {
-		/*---------------------------------
-		 * In this file, you will recreate 
-		 * the age verification program we
-		 * created in class. When you're
-		 * finished, delete these instructions
-		 * before submitting.
-		 ---------------------------------*/
-
+		//Declaring and initializing variables with output
+		String firstName = JOptionPane.showInputDialog(null, "What is your first name?"); 
+		String lastName = JOptionPane.showInputDialog(null, "What is your last name?"); 
+		int yearOfBirth = Integer.parseInt(JOptionPane.showInputDialog(null, "What year were you born?")); 
+		
+		boolean oldEnough = verifyLegalAge(yearOfBirth); 
+		String fullName = concatenateNames(firstName, lastName);
+		
+		//if statement for boolean logic 
+		if(oldEnough == true) {
+			JOptionPane.showMessageDialog(null, fullName + ", you may proceed with your shopping.");
+		} else {
+			JOptionPane.showMessageDialog(null, fullName + ", you are not old enough");
+		}
+		
 	}
-
+	
+	//Create method for verifying age 
+	public static boolean verifyLegalAge(int yob) {
+		boolean legalAgeStatus = (2013 - yob ) >= 19;
+		return legalAgeStatus; 
+	}
+	
+	//Create method for full name
+	public static String concatenateNames(String fn, String ln) {
+		String fullName = (fn) + (ln); 
+		return fullName; 
+	}
 }
